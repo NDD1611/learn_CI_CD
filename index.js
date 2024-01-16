6a4ce4f33878e16e59eabfcb528899a668ae1394
nguyendinhdinh16112000@gmail.com <nguyendinhdinh16112000@gmail.com>
2024-01-16 14:15:20 +0700

    ADD: router /api/data

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-app.get('/data', (req, res) => {
+app.get('/api/data', (req, res) => {
     res.json({
         name: 'Nguyễn Đình Đình',
         age: 23,
